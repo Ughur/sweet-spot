d727a70bfb10dec6c504386a60aff6d2427aa78a
Ughur <ughurnn@gmail.com>
2025-03-03 06:23:28 -0800

    format code

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,10 +1,14 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { Cart } from "../types/cart";
-import ApiClient from "../services/api-client";
+import {
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
+import { Cart } from '../types/cart';
+import ApiClient from '../services/api-client';
 
 const useCart = () => {
   const queryClient = useQueryClient();
-  const apiClient = new ApiClient<Cart>('/cart')
+  const apiClient = new ApiClient<Cart>('/cart');
 
   const { data: cart, isLoading } = useQuery({
     queryKey: ['cart'],
@@ -14,68 +18,108 @@ const useCart = () => {
   const addToCart = useMutation({
     mutationFn: apiClient.create,
     onMutate: (newItem) => {
-      queryClient.cancelQueries({ queryKey: ['cart'] })
+      queryClient.cancelQueries({ queryKey: ['cart'] });
 
-      const previousCarts = queryClient.getQueryData<Cart[]>(['cart'])
-      const optimisticUpdate = [...(previousCarts ?? []), newItem]
+      const previousCarts = queryClient.getQueryData<
+        Cart[]
+      >(['cart']);
+      const optimisticUpdate = [
+        ...(previousCarts ?? []),
+        newItem,
+      ];
 
-      queryClient.setQueryData(['cart'], () => optimisticUpdate)
+      queryClient.setQueryData(
+        ['cart'],
+        () => optimisticUpdate
+      );
 
-      return { previousCarts }
+      return { previousCarts };
     },
     onError: (_error, _newItem, context) => {
-      queryClient.setQueryData(['cart'], context?.previousCarts)
+      queryClient.setQueryData(
+        ['cart'],
+        context?.previousCarts
+      );
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['cart'] })
-    }
-  })
+      queryClient.invalidateQueries({ queryKey: ['cart'] });
+    },
+  });
 
   const updateCart = useMutation({
-    mutationFn: ({ id, data }: { id: number; data: Partial<Cart> }) => apiClient.update(id, data),
+    mutationFn: ({
+      id,
+      data,
+    }: {
+      id: number;
+      data: Partial<Cart>;
+    }) => apiClient.update(id, data),
     onMutate: ({ id, data }) => {
-      queryClient.cancelQueries({ queryKey: ['cart'] })
+      queryClient.cancelQueries({ queryKey: ['cart'] });
 
-      const previousCarts = queryClient.getQueryData<Cart[]>(['cart'])
-      const optimisticUpdate = previousCarts?.map((cartItem) => cartItem.id === id ? { ...cartItem, ...data } : cartItem)
+      const previousCarts = queryClient.getQueryData<
+        Cart[]
+      >(['cart']);
+      const optimisticUpdate = previousCarts?.map(
+        (cartItem) =>
+          cartItem.id === id
+            ? { ...cartItem, ...data }
+            : cartItem
+      );
 
-      queryClient.setQueryData(['cart'], () => optimisticUpdate)
-      return { previousCarts }
+      queryClient.setQueryData(
+        ['cart'],
+        () => optimisticUpdate
+      );
+      return { previousCarts };
     },
     onError: (_error, _newItem, context) => {
-      queryClient.setQueryData(['cart'], context?.previousCarts)
+      queryClient.setQueryData(
+        ['cart'],
+        context?.previousCarts
+      );
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['cart'] })
-    }
-  })
+      queryClient.invalidateQueries({ queryKey: ['cart'] });
+    },
+  });
 
   const deleteCart = useMutation({
     mutationFn: apiClient.delete,
     onMutate: (id) => {
-      queryClient.cancelQueries({ queryKey: ['cart'] })
+      queryClient.cancelQueries({ queryKey: ['cart'] });
 
-      const previousCarts = queryClient.getQueryData<Cart[]>(['cart'])
-      const optimisticUpdate = previousCarts?.filter((cartItem) => cartItem.id !== id)
+      const previousCarts = queryClient.getQueryData<
+        Cart[]
+      >(['cart']);
+      const optimisticUpdate = previousCarts?.filter(
+        (cartItem) => cartItem.id !== id
+      );
 
-      queryClient.setQueryData(['cart'], () => optimisticUpdate)
-      return { previousCarts }
+      queryClient.setQueryData(
+        ['cart'],
+        () => optimisticUpdate
+      );
+      return { previousCarts };
     },
     onError: (_error, _newItem, context) => {
-      queryClient.setQueryData(['cart'], context?.previousCarts)
+      queryClient.setQueryData(
+        ['cart'],
+        context?.previousCarts
+      );
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['cart'] })
-    }
-  })
+      queryClient.invalidateQueries({ queryKey: ['cart'] });
+    },
+  });
 
   return {
     cart,
     isLoading,
     addToCart,
     updateCart,
-    deleteCart
-  }
-}
+    deleteCart,
+  };
+};
 
 export default useCart;
